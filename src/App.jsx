dc24b3c0e551bc89506bf2781752456476e13f1d
JohnSamehRoshdy <john.sameh13@gmail.com>
2024-12-29 08:12:22 -0800

    layout of project pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import "./App.css";
 import Home from "./Home/Home";
 
 function App() {
-  // const sidebarTabs = [{ label: "", link: " ", icon: "" }];
-  return <Home />;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
