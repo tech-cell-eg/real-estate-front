f13758972f0d448d5da2c0b682a27e18f427e2c7
Marwaa <meroblue25@gmail.com>
2024-12-29 18:29:27 +0200

    solve conflict between layout and company file names

--- src/Company/Pages/Home/Home.jsx
+++ src/Company/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Home() {
+export default function CompanyHome() {
   return<>
   <section className='w-[90%] m-auto'>
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
