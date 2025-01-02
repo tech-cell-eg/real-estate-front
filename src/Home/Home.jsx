5a3163dff1dd80bb95881edb788e77a86531ae60
JohnSamehRoshdy <john.sameh13@gmail.com>
2025-01-02 15:42:18 -0800

    Adding ContactUs Section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,18 @@
 import About from "./About";
 import AppPromotion from "./AppPromotion";
+import ContactUs from "./ContactUs";
 import HeroSection from "./HeroSection";
+import Slider from "./Slider";
 
 function Home() {
   return (
-    <>
+    <div className=" bg-[#F7FBF9]">
       <HeroSection />
       <About />;
+      <Slider />
+      <ContactUs />
       <AppPromotion />
-    </>
+    </div>
   );
 }
 
