78e6ceab20630c3a2f72c9f49329e7a8ffa65e42
ayaFarh <farhaya004@gmail.com>
2025-01-05 18:47:06 +0200

    update team

--- src/Company/Pages/Team/TeamCard.jsx
+++ src/Company/Pages/Team/TeamCard.jsx
@@ -1,35 +1,46 @@
-import React, { useState } from 'react'
-import EditePoth from './EditePoth'
+import React from "react";
 
-export default function TeamCard() {
-    const [showEdite,setShowEdite]=useState(false)
+export default function TeamCard({ setShowEdite }) {
+  const handleShowEdite = () => {
+    setShowEdite((prev) => !prev);
+  };
 
-    const handleShowEdite=()=>{
-        setShowEdite(!showEdite)
-    }
-  return <>
-  
-<div className='relative rounded-xl shadow-2xl border border-gray-300 p-6 space-y-4 mt-6 '>
-<div className='flex items-center gap-4 '>
-    <div>
-        <img src='/v987-09.jpg' alt='' className='w-24 h-24 rounded object-cover' />
-    </div>
-    <div>
-        <h2>اسم الموظف</h2>
-        <p>الوصف</p>
-    </div>
-</div>
+  return (
+    <>
+      <div className="rounded-xl shadow-2xl border border-gray-300 p-6 space-y-4 mt-6">
+        <div className="relative flex items-center gap-4">
+          <div>
+            <img
+              src="/v987-09.jpg"
+              alt="صورة"
+              className="w-24 h-24 rounded object-cover"
+            />
+          </div>
+          <div>
+            <h2>اسم الموظف</h2>
+            <p>الوصف</p>
+          </div>
 
-<button className='bg-primary text-white py-2 px-4 rounded w-full' onClick={handleShowEdite}>تعديل </button>
-<div className='flex justify-between gap-4 max-[345px]:flex-col'>
-<button className='w-1/2 max-[345px]:w-full bg-primary text-white py-2 px-4 rounded '>حذف</button>
-<button className= 'w-1/2 max-[345px]:w-full text-nowrap  bg-primary text-white py-2 px-4 rounded'>ارسال البيانات </button>
-<div className='absolute top-4 left-4 bg-primary text-white py-2 px-6 rounded-full flex items-center justify-center'><span>مدقق</span></div>
-</div>
+          <div className="absolute -top-3 -left-4 bg-primary text-white py-2 px-6 rounded-full flex items-center justify-center">
+            <span>مدقق</span>
+          </div>
+        </div>
 
-{showEdite && (<EditePoth/>)}
-</div>
- 
-  
-  </>
+        <button
+          className="bg-primary text-white py-2 px-4 rounded w-full"
+          onClick={handleShowEdite}
+        >
+          تعديل
+        </button>
+        <div className="flex justify-between gap-4 max-[345px]:flex-col">
+          <button className="w-1/2 max-[345px]:w-full bg-primary text-white py-2 px-4 rounded">
+            حذف
+          </button>
+          <button className="w-1/2 max-[345px]:w-full text-nowrap bg-primary text-white py-2 px-4 rounded">
+            ارسال البيانات
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
