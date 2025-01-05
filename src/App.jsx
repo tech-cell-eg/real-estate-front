f208a7cc3b780e4201e77555f8e268b6a9092ce7
JohnSamehRoshdy <john.sameh13@gmail.com>
2025-01-05 13:06:50 -0800

    UI issues in HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import "./App.css";
 import Home from "./Home/Home";
 import CompanyHome from "./Company/Pages/Home/Home";
 import clientRoute from "./client/routes/ClientRoute";
+import Login from "./Auth/components/Login";
 
 // ! You can find an example for routing for your flow in company role
 
@@ -17,7 +18,7 @@ export default function App() {
       path: "/",
       children: [
         { index: true, element: <Home /> },
-        // { path: "login", element: <Login /> },
+        { path: "login", element: <Login /> },
         // { path: "register", element: <Register /> },
       ],
       errorElement: <ErrorElement />,
--- src/Home/ContactUs.jsx
+++ src/Home/ContactUs.jsx
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import { FaPhoneAlt, FaFax, FaEnvelope } from "react-icons/fa"; // Importing icons from react-icons
+import { FiPhoneCall } from "react-icons/fi";
+import { MdOutlineFax } from "react-icons/md";
+import { FaEnvelopeOpenText } from "react-icons/fa6";
+import { useState } from "react";
 
 const ContactUs = () => {
   const [email, setEmail] = useState("");
@@ -43,7 +47,10 @@ const ContactUs = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row items-center md:items-start md:justify-between px-4 py-8 md:px-16">
+    <div
+      id="contact"
+      className="flex flex-col md:flex-row items-center md:items-start md:justify-between px-4 py-8 md:px-16"
+    >
       <div className="w-full md:w-1/2 mt-8 md:mt-0 flex justify-center">
         <img
           src="./photos/map.png"
@@ -55,65 +62,100 @@ const ContactUs = () => {
         onSubmit={handleSubmit}
         className="w-full md:w-1/2 space-y-4 bg-white p-6 rounded shadow"
       >
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-4xl font-bold mb-8">
           <span>
-            <span className="text-orange-500"> هيا</span>
+            <span className="text-[rgba(194,144,98,1)]">هيا &nbsp;</span>
             نتواصل !
           </span>
         </h1>
-        <p className="text-gray-600">
-          يمكنك التواصل معنا من خلال ترك رسالة وسيتم التواصل معك لاحقًا.
+        <p className="text-gray-600 mb-8">
+          يمكنك التواصل معنا من خلال ترك رسالة و أحد موظفي خدمة العملاء سيتواصل
+          معك لاحقَا{" "}
         </p>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Name"
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-orange-500 text-left"
-          />
-        </div>
+        <div className="mt-8 space-y-6">
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-[rgba(194,144,98,1)] text-left"
+            />
+          </div>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-orange-500 text-left"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm">{errors.email}</p>
-          )}
-        </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-[rgba(194,144,98,1)] text-left"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm">{errors.email}</p>
+            )}
+          </div>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Phone number"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-orange-500 text-left"
-          />
-          {errors.phone && (
-            <p className="text-red-500 text-sm">{errors.phone}</p>
-          )}
-        </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Phone number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-[rgba(194,144,98,1)] text-left"
+            />
+            {errors.phone && (
+              <p className="text-red-500 text-sm">{errors.phone}</p>
+            )}
+          </div>
 
-        <div>
-          <textarea
-            placeholder="Your Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-orange-500 text-left"
-          />
+          <div>
+            <textarea
+              placeholder="Your Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full border border-gray-300 p-2 rounded focus:outline-none focus:ring focus:ring-[rgba(194,144,98,1)] text-left"
+            />
+          </div>
         </div>
 
         <button
           type="submit"
-          className="w-full bg-orange-500 text-white p-2 rounded hover:bg-orange-600"
+          className="w-full bg-[rgba(194,144,98,1)] text-white p-2 rounded hover:bg-[#d5b496]"
         >
           إرسال
         </button>
+
+        <div className=" flex justify-center  gap-16 items-center mx-auto">
+          <div className="flex items-center gap-4">
+            <div>
+              <p className="font-bold text-left">البريد الالكترونى</p>
+              <p>1234 5432 03</p>
+            </div>
+            <div>
+              <FaEnvelopeOpenText className="text-[rgba(194,144,98,1)] text-2xl mb-2" />
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div>
+              <p className="font-bold text-left">فاكس</p>
+              <p>1234 5432 03</p>
+            </div>
+            <div>
+              <MdOutlineFax className="text-[rgba(194,144,98,1)] text-2xl mb-2" />
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <div>
+              <p className="font-bold text-left text-[rgba(194,144,98,1)]">
+                الهاتف
+              </p>
+              <p>1234 5432 03</p>
+            </div>
+            <div>
+              <FiPhoneCall className="text-[rgba(194,144,98,1)] text-2xl mb-2" />
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   );
